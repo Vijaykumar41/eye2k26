4af46bb256cb11d0f3900add03f28e1126d5598f
Vijaykumar41 <131332833+Vijaykumar41@users.noreply.github.com>
2026-02-15 00:11:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,8 +72,9 @@ def ensure_headers():
 # ==============================
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
-EMAIL_ADDRESS = os.environ.get("EMAIL_USER")
-EMAIL_PASSWORD = os.environ.get("EMAIL_PASS")
+EMAIL_ADDRESS = os.environ.get("officialeye2k26@gmail.com
+")
+EMAIL_PASSWORD = os.environ.get("eyxs kczc fdgh upbo")
 
 # ==============================
 # HELPER FUNCTIONS
